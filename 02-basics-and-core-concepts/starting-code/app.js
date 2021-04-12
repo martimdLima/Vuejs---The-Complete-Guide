d47d76d497d07b950aab6c4f7c315d5edbcb7a54
Martim Lima <martim.d.lima@protonmail.com>
2021-04-12 13:13:02 +0100

    Basics & Core Concepts - DOM Interaction with Vue
    Binding Attributes with the "v-bind" Directive

--- 02-basics-and-core-concepts/starting-code/app.js
+++ 02-basics-and-core-concepts/starting-code/app.js
@@ -5,6 +5,17 @@ const app = Vue.createApp({
             vueLink: "https://vuejs.org",
         };
     },
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();
+
+            if (randomNumber < 0.5) {
+                return "Test Case 1";
+            } else {
+                return "Test Case 2";
+            }
+        },
+    },
 });
 
 app.mount("#user-goal");
