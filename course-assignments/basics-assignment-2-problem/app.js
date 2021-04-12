6956899901e7acf2512679e436713d2087101b47
Martim Lima <martim.d.lima@protonmail.com>
2021-04-12 16:43:20 +0100

    Basics & Core Concepts - DOM Interaction with Vue
    Course Assigment 2 - Event Binding

---
+++ course-assignments/basics-assignment-2-problem/app.js
@@ -0,0 +1,18 @@
+const app = Vue.createApp({
+    data() {
+      return { userInput: '', confirmedUserInput: '' };
+    },
+    methods: {
+      showAlert() {
+        alert('Test Alert');
+      },
+      setUserInput(event) {
+        this.userInput = event.target.value;
+      },
+      confirmUserInput() {
+        this.confirmedUserInput = this.userInput;
+      }
+    }
+  });
+  
+  app.mount('#assignment'); 
