25d94f335ff7acbdb1e8adeb3329fe2829d999ee
Martim Lima <martim.d.lima@protonmail.com>
2021-04-12 12:31:23 +0100

    Getting Started
    Re-building the App with Vue

--- 01-getting-started/01-starting-project/app.js
+++ 01-getting-started/01-starting-project/app.js
@@ -1,4 +1,5 @@
-const buttonEl = document.querySelector('button');
+// Imperative Approach
+/* const buttonEl = document.querySelector('button');
 const inputEl = document.querySelector('input');
 const listEl = document.querySelector('ul');
 
@@ -11,4 +12,19 @@ function addGoal() {
     inputEl.value = "";
 }
 
-buttonEl.addEventListener('click', addGoal);
+buttonEl.addEventListener('click', addGoal); */
+
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: "",
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            this.enteredValue = "";
+        },
+    },
+}).mount("#app");
