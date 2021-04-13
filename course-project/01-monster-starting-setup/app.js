911684712687d9b4dad745349aea1e2571666521
Martim Lima <martim.d.lima@protonmail.com>
2021-04-13 10:51:50 +0100

    Course Project: The Monster Slayer Game
    Updating the Health Bars

--- course-project/01-monster-starting-setup/app.js
+++ course-project/01-monster-starting-setup/app.js
@@ -1,3 +1,35 @@
-const app = Vue.createApp({});
+function getRandomValue(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+const app = Vue.createApp({
+    data() {
+        return {
+            playerHealth: 100,
+            playerAttack: 15,
+            monsterHealth: 100,
+            monsterAttack: 5,
+        };
+    },
+    computed: {
+        monsterBarStyles() {
+            return {width: this.monsterHealth + '%'}
+        },
+        playerBarStyles() {
+            return {width: this.playerHealth + '%'}
+        }
+    },
+    methods: {
+        attackMonster() {
+            const attackDamage = getRandomValue(5, 12);
+            this.monsterHealth -= attackDamage;
+            this.attackPlayer();
+        },
+        attackPlayer() {
+            const attackDamage = getRandomValue(8, 15);
+            this.playerHealth -= attackDamage;
+        },
+    },
+});
 
 app.mount("#game");
