ca364b5a10716b42e4b5f6d08e3b4c00eed47af7
Martim Lima <martim.d.lima@protonmail.com>
2021-04-13 16:22:39 +0100

    Moving to a Better Development Setup & Workflow with the Vue CLI
    Creating a Basic Vue App

---
+++ course-modules/06-moving-to-a-better-development-setup/02-a-new-vue-project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<section>
+ <h1>Friends List</h1>
+     <ul>
+        <li></li>
+      </ul>
+</section> 
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "tf1",
+          name: "John Doe",
+          phone: 953283454,
+          email: "test@test.com",
+        },
+        {
+          id: "tf2",
+          name: "Jane Doe",
+          phone: 954853214,
+          email: "test@test.com",
+        },
+      ],
+    };
+  },
+};
+</script>
