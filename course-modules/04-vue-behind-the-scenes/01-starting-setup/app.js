d9766828c6c32ea4316a8ab210c93db7fca39854
Martim Lima <martim.d.lima@protonmail.com>
2021-04-13 14:10:09 +0100

    Vue: Behind the Scenes
    Vue App Lifecycle - Practice

--- course-modules/04-vue-behind-the-scenes/01-starting-setup/app.js
+++ course-modules/04-vue-behind-the-scenes/01-starting-setup/app.js
@@ -1,32 +1,62 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      currentUserInput: '',
-      message: 'Vue is great!',
-    };
-  },
-  methods: {
-    saveInput(event) {
-      this.currentUserInput = event.target.value;
+    data() {
+        return {
+            currentUserInput: "",
+            message: "Vue is great!",
+        };
     },
-    setText() {
-      //this.message = this.currentUserInput;
-      this.message = this.$refs.userText.value
+    methods: {
+        saveInput(event) {
+            this.currentUserInput = event.target.value;
+        },
+        setText() {
+            //this.message = this.currentUserInput;
+            this.message = this.$refs.userText.value;
+        },
     },
-  },
+    beforeCreate() {
+        console.log("beforeCreate()");
+    },
+    created() {
+        console.log("created()");
+    },
+    beforeMount() {
+        console.log("beforeMount()");
+    },
+    mounted() {
+        console.log("mounted()");
+    },
+    beforeUpdate() {
+        console.log("beforeUpdate()");
+    },
+    updated() {
+        console.log("updated()");
+    },
+    beforeUnmount() {
+        console.log("beforeUnmount() ");
+    },
+    unmounted() {
+        console.log("unmounted()");
+    },
 });
 
-app.mount('#app');
+app.mount("#app");
+
+setTimeout(() => {
+    app.unmount();
+}, 3000);
 
 const app2 = Vue.createApp({
-/*   template: `
+    /*   template: `
     <p>{{ favoriteMeal }}</p>
   `, */
-  data() {
-    return {
-      favoriteMeal: 'Pizza'
-    };
-  }
+    data() {
+        return {
+            favoriteMeal: "Pizza",
+        };
+    },
 });
 
-app2.mount('#app2');
+app2.mount("#app2");
+
+app2.unmount();
