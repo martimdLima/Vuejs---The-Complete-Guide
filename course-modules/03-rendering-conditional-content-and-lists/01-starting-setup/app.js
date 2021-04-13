f72cfda02872a4394e706720e72d4eedb84035b8
Martim Lima <martim.d.lima@protonmail.com>
2021-04-13 08:58:50 +0100

    Rendering Conditional Content & Lists
    Rendering Content Conditionally

---
+++ course-modules/03-rendering-conditional-content-and-lists/01-starting-setup/app.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    data() {
+        return { goals: [], enteredGoal: "" };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredGoal);
+        },
+    },
+});
+
+app.mount("#user-goals");
