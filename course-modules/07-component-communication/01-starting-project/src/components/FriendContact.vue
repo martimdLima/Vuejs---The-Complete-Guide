5b11420566419dfb92d963d1f8e94b64a908033f
Martim Lima <martim.d.lima@protonmail.com>
2021-04-13 17:13:45 +0100

    Component Communication
     Introducing "Props" (Parent => Child Communication)

---
+++ course-modules/07-component-communication/01-starting-project/src/components/FriendContact.vue
@@ -0,0 +1,42 @@
+<template>
+  <ul>
+    <li>
+      <h2>{{ name }}</h2>
+      <button @click="toggleDetails">
+        {{ detailsAreVisible ? "Hide" : "Show" }} Details
+      </button>
+      <ul v-if="detailsAreVisible">
+        <li>
+          <strong>Phone:</strong>
+          {{ phoneNumber }}
+        </li>
+        <li>
+          <strong>Email:</strong>
+          {{ emailAddress }}
+        </li>
+      </ul>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ["name", "phoneNumber", "emailAddress"],
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "tf1",
+        name: "John Doe",
+        phone: "953283454",
+        email: "test@test.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
