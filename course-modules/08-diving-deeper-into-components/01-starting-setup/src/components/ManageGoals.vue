62f1ea07febdff2d4a4f22f9ee97febeea63522b
Martim Lima <martim.d.lima@protonmail.com>
2021-04-14 16:24:00 +0100

    Diving Deeper Into Components
    Applying What We Know & A Problem

--- course-modules/08-diving-deeper-into-components/01-starting-setup/src/components/ManageGoals.vue
+++ course-modules/08-diving-deeper-into-components/01-starting-setup/src/components/ManageGoals.vue
@@ -1,7 +1,38 @@
 <template>
-<div>
+  <div>
     <h2>Manage Goals</h2>
-    <input type="text" placeholder="Goal"/>
-</div>
-  
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+    <error-alert v-if="inputIsInvalid">
+      <h2>Input is invalid</h2>
+      <p>Please enter a few characters</p>
+      <button @click="confirmError">Okay</button>
+    </error-alert>
+  </div>
 </template>
+
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+
+export default {
+  components: { ErrorAlert },
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+
+      if (enteredValue === "") {
+        //alert("Input must not be empty!");
+        this.inputIsInvalid = true;
+      }
+    },
+    confirmError() {
+      this.inputIsInvalid = !this.inputIsInvalid;
+    },
+  },
+};
+</script>
