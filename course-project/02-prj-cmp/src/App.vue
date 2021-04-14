fe7d3d310f7533f8e6d539df2253e177cc11814b
Martim Lima <martim.d.lima@protonmail.com>
2021-04-14 17:40:51 +0100

    Course Project: The Learning Resources App
    Setup & First Steps
    First Components & Props

---
+++ course-project/02-prj-cmp/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
+export default {
+  components: {
+    LearningResource
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Offical Guide',
+          description: 'The Official Vue.js Documentation',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Google',
+          link: 'https://google.com'
+        }
+      ]
+    };
+  }
+};
+</script>
