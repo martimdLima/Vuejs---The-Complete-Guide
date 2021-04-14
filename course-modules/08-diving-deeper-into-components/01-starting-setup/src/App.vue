fa051ebd78601cafae2d8dd07a9c2855ed60546b
Martim Lima <martim.d.lima@protonmail.com>
2021-04-14 13:42:25 +0100

    Diving Deeper Into Components
    Global vs Local Components

---
+++ course-modules/08-diving-deeper-into-components/01-starting-setup/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+
+    <TheHeader />
+    <BadgeList />
+    <UserInfo full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role" />
+<!-- <the-header></the-header>
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info> -->
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader";
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+
+export default {
+  /* components: { "the-header": TheHeader }, */
+  components: {TheHeader, BadgeList, UserInfo},
+  data() {
+    return {
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
