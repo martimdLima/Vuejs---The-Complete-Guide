b7fb76ea2dae8886945f8c5b17b6fcb45c049c9c
Martim Lima <martim.d.lima@protonmail.com>
2021-04-14 14:46:02 +0100

    Diving Deeper Into Components
    Named Slots

--- course-modules/08-diving-deeper-into-components/01-starting-setup/src/components/BaseCard.vue
+++ course-modules/08-diving-deeper-into-components/01-starting-setup/src/components/BaseCard.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
-    <slot></slot>
-  </div>
+    <div>
+        <header><slot name="header"></slot></header>
+        <slot></slot>
+    </div>
 </template>
 
 <script>
@@ -10,10 +11,10 @@ export default {};
 
 <style scoped>
 div {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
+    margin: 2rem auto;
+    max-width: 30rem;
+    border-radius: 12px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    padding: 1rem;
 }
 </style>
