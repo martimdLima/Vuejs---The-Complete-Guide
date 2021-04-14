a290a52ee693b2947e5d32de6b36f605b55d916f
Martim Lima <martim.d.lima@protonmail.com>
2021-04-14 12:12:10 +0100

    Component Communication
    Course Assignment - Props & Custom Events

---
+++ course-assignments/04-component-comunication/cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,36 @@
+<template>
+  <section>
+    <ul>
+      <li>
+        <strong>Name:</strong>
+        {{ username }}
+      </li>
+      <li>
+        <strong>Age:</strong>
+        {{ age }}
+      </li>
+    </ul>
+  </section>
+</template>
+
+
+<script>
+export default {
+  props: {
+    id: { type: String, required: true },
+    username: { type: String, required: true },
+    age: { type: String, required: true },
+  },
+  data() {
+    return {
+      user: {
+        id: new Date("2020/04/14").toISOString(),
+        username: "test",
+        age: "32",
+      },
+    };
+  },
+};
+</script>
+
+// ActiveUser should output a username (h2) and age (h3)
---
+++ course-assignments/04-component-comunication/cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,35 @@
+// UserData should output two input fields => for name and age
+<template>
+  <form @submit.prevent="submitData()">
+    <div>
+      <label>Name</label>
+      <input type="text" placeholder="Name" v-model="usernameInput" />
+    </div>
+    <div>
+      <label>Age</label>
+      <input type="text" placeholder="Age" v-model="ageInput" />
+    </div>
+    <div>
+      <button>Change User</button>
+    </div>
+  </form>
+</template>
+
+
+
+<script>
+export default {
+  emits: ["change-user"],
+  data() {
+    return {
+      usernameInput: "",
+      ageInput: "",
+    };
+  },
+  methods: {
+    submitData() {
+      this.$emit("change-user", this.usernameInput, this.ageInput);
+    },
+  },
+};
+</script>
