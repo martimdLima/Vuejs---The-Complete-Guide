5d3ec33c98782b614316d6d9a6fd9cf8e907a5be
Martim Lima <martim.d.lima@protonmail.com>
2021-04-15 16:49:49 +0100

    Animations & Transitions
    CSS Animations with Vue's Help

--- course-modules/12-animations/01-starting-setup/src/App.vue
+++ course-modules/12-animations/01-starting-setup/src/App.vue
@@ -3,6 +3,12 @@
     <div class="block" :class="{ animate: animatedBlock }"></div>
     <button @click="animateBlock">Animate</button>
   </div>
+  <div class="container">
+    <transition>
+      <p v-if="paraIsVisible">This is only sometimes visible</p>
+    </transition>
+    <button @click="toggleParagraph">Toggle Paragraph</button>
+  </div>
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
@@ -17,7 +23,8 @@ export default {
   data() {
     return {
       animatedBlock: false,
-      dialogIsVisible: false
+      dialogIsVisible: false,
+      paraIsVisible: false
     };
   },
   methods: {
@@ -29,6 +36,9 @@ export default {
     },
     hideDialog() {
       this.dialogIsVisible = false;
+    },
+    toggleParagraph() {
+      this.paraIsVisible = !this.paraIsVisible;
     }
   }
 };
