2150946cc3fede453ce415c44d579b604b3243f7
Martim Lima <martim.d.lima@protonmail.com>
2021-04-16 09:31:22 +0100

    Animations & Transitions
    Using Custom CSS Class Names
    Example: Animating a Modal

--- course-modules/12-animations/01-starting-setup/src/App.vue
+++ course-modules/12-animations/01-starting-setup/src/App.vue
@@ -4,12 +4,13 @@
     <button @click="animateBlock">Animate</button>
   </div>
   <div class="container">
-    <transition>
+    <transition name="para">
       <p v-if="paraIsVisible">This is only sometimes visible</p>
     </transition>
     <button @click="toggleParagraph">Toggle Paragraph</button>
   </div>
-  <base-modal @close="hideDialog" v-if="dialogIsVisible">
+
+  <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
   </base-modal>
@@ -88,39 +89,38 @@ button:active {
 }
 .animate {
   /* transform: translateX(-150px); */
-  animation: slide-fade 0.3s ease-out forwards;
+  animation: slide-scale 0.3s ease-out forwards;
 }
 
-.v-enter-from {
+.para-enter-from {
   /*     opacity: 0;
   transform: translateY(-30px); */
 }
 
-.v-enter-active {
+.para-enter-active {
   /* transition: all 0.3s ease-out; */
   animation: slide-scale 0.3s ease-out;
 }
 
-.v-enter-to {
+.para-enter-to {
   /*      opacity: 1;
   transform: translateY(0);  */
 }
 
-.v-leave-from {
+.para-leave-from {
   /*   opacity: 1;
   transform: translateY(0); */
 }
 
-.v-leave-active {
+.para-leave-active {
   /* transition: all 0.3s ease-in; */
   animation: slide-scale 0.3s ease-out;
 }
 
-/*
-.v-leave-to {
-  opacity: 0;
-  transform: translateY(30px);
-}*/
+.para-leave-to {
+  /*   opacity: 0;
+  transform: translateY(30px); */
+}
 
 /*  CSS Animations */
 @keyframes slide-scale {
