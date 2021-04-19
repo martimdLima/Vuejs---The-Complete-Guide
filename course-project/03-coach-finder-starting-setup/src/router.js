8a6fc2746f7ccb13aadf0e1427938916547c957c
Martim Lima <martim.d.lima@protonmail.com>
2021-04-19 09:58:17 +0100

    Main Project: "Find a Coach" Web App
    Wiring Up Pages

--- course-project/03-coach-finder-starting-setup/src/router.js
+++ course-project/03-coach-finder-starting-setup/src/router.js
@@ -1,12 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachesList from './pages/coaches/CoachesList.vue';
 import CoachDetail from './pages/coaches/CoachDetail.vue';
-import CoachRegistration from './pages/coaches/CoachRegistration.vue';
-
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegistation from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
-
 import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
@@ -15,11 +13,13 @@ const router = createRouter({
     { path: '/', redirect: '/coaches' },
     { path: '/coaches', component: CoachesList },
     {
-      path: '/coaches/id',
+      path: '/coaches/:id',
       component: CoachDetail,
-      children: [{ path: 'contact', component: ContactCoach }]
+      children: [
+        { path: 'contact', component: ContactCoach } // /coaches/c1/contact
+      ]
     },
-    { path: '/register', component: CoachRegistration },
+    { path: '/register', component: CoachRegistation },
     { path: '/requests', component: RequestsReceived },
     { path: '/:notFound(.*)', component: NotFound }
   ]
