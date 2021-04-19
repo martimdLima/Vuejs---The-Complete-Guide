f1da6bec5ea565c2c6d216e23d645b6e5f5f87bb
Martim Lima <martim.d.lima@protonmail.com>
2021-04-19 12:03:03 +0100

    Main Project: "Find a Coach" Web App
    Adding Coaches to Vuex

--- course-project/03-coach-finder-starting-setup/src/store/modules/coaches/actions.js
+++ course-project/03-coach-finder-starting-setup/src/store/modules/coaches/actions.js
@@ -1,3 +1,14 @@
-export default{
-    
-}
+export default {
+  registerCoach(context, payload) {
+    const coach = {
+      id: context.rootGetters.userId,
+      firstName: payload.first,
+      lastName: payload.last,
+      description: payload.desc,
+      hourlyRate: payload.rate,
+      areas: payload.areas
+    };
+
+    context.commit('registerCoach', coach);
+  }
+};
--- course-project/03-coach-finder-starting-setup/src/store/modules/coaches/mutations.js
+++ course-project/03-coach-finder-starting-setup/src/store/modules/coaches/mutations.js
@@ -1,3 +1,5 @@
-export default{
-    
-}
+export default {
+  registerCoach(state, payload) {
+    state.coaches.push(payload);
+  }
+};
