c03ac8e8c359aff338518ab2935c42032ae02dcb
Martim Lima <martim.d.lima@protonmail.com>
2021-04-19 10:27:37 +0100

    Main Project: "Find a Coach" Web App
    Working on the Coaches List and List Items

--- course-project/03-coach-finder-starting-setup/src/pages/coaches/CoachesList.vue
+++ course-project/03-coach-finder-starting-setup/src/pages/coaches/CoachesList.vue
@@ -6,16 +6,28 @@
       <router-link to="/register">Register as Coach</router-link>
     </div>
     <ul v-if="hasCoaches">
-      <li v-for="coach in filteredCoaches" :key="coach.id">
+      <coach-item
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :firstName="coach.firstName"
+        :lastName="coach.lastName"
+        :rate="coach.hourlyRate"
+        :areas="coach.areas"
+      ></coach-item>
+      <!--       <li v-for="coach in filteredCoaches" :key="coach.id">
         {{ coach.firstName }}
-      </li>
+      </li> -->
     </ul>
     <h3 v-else>No Coaches found</h3>
   </section>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
+
 export default {
+  components: { CoachItem },
   computed: {
     filteredCoaches() {
       return this.$store.getters['coaches/coaches'];
@@ -26,3 +38,16 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
