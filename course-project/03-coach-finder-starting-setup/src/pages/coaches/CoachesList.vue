ad0746d7ee81a2156417fab425b0e8139d7ee004
Martim Lima <martim.d.lima@protonmail.com>
2021-04-19 10:49:46 +0100

    Main Project: "Find a Coach" Web App
    Adding a Couple of Base Components (Base Card, Base
    Button, Base Badge)

--- course-project/03-coach-finder-starting-setup/src/pages/coaches/CoachesList.vue
+++ course-project/03-coach-finder-starting-setup/src/pages/coaches/CoachesList.vue
@@ -1,25 +1,27 @@
 <template>
   <section>FILTER</section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as Coach</router-link>
-    </div>
-    <ul v-if="hasCoaches">
-      <coach-item
-        v-for="coach in filteredCoaches"
-        :key="coach.id"
-        :id="coach.id"
-        :firstName="coach.firstName"
-        :lastName="coach.lastName"
-        :rate="coach.hourlyRate"
-        :areas="coach.areas"
-      ></coach-item>
-      <!--       <li v-for="coach in filteredCoaches" :key="coach.id">
+    <base-card>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link to="/register">Register as Coach</base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :firstName="coach.firstName"
+          :lastName="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+        ></coach-item>
+        <!--       <li v-for="coach in filteredCoaches" :key="coach.id">
         {{ coach.firstName }}
       </li> -->
-    </ul>
-    <h3 v-else>No Coaches found</h3>
+      </ul>
+      <h3 v-else>No Coaches found</h3>
+    </base-card>
   </section>
 </template>
 
