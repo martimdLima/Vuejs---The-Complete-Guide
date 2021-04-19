1468207544fa146fc55893a0c5b5ea5c32a98be7
Martim Lima <martim.d.lima@protonmail.com>
2021-04-19 12:35:49 +0100

    Main Project: "Find a Coach" Web App
    Adding Form Validation

--- course-project/03-coach-finder-starting-setup/src/pages/coaches/CoachesList.vue
+++ course-project/03-coach-finder-starting-setup/src/pages/coaches/CoachesList.vue
@@ -44,20 +44,16 @@ export default {
   computed: {
     filteredCoaches() {
       const coaches = this.$store.getters['coaches/coaches'];
-
       return coaches.filter(coach => {
         if (this.activeFilters.frontend && coach.areas.includes('frontend')) {
           return true;
         }
-
         if (this.activeFilters.backend && coach.areas.includes('backend')) {
           return true;
         }
-
-        if (this.activeFilters.careers && coach.areas.includes('career')) {
+        if (this.activeFilters.career && coach.areas.includes('career')) {
           return true;
         }
-
         return false;
       });
     },
