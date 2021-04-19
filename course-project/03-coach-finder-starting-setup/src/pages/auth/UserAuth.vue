af1fb51b861101ba693d20e5b2afd73ec45ca4a0
Martim Lima <martim.d.lima@protonmail.com>
2021-04-19 17:27:10 +0100

    Vue & Authentication
    Better UX: Loading Spinner & Error Handling

--- course-project/03-coach-finder-starting-setup/src/pages/auth/UserAuth.vue
+++ course-project/03-coach-finder-starting-setup/src/pages/auth/UserAuth.vue
@@ -1,21 +1,32 @@
 <template>
-  <base-card>
-    <form @submit.prevent="submitForm">
-      <div class="form-control">
-        <label for="email">E-Mail</label>
-        <input type="email" id="email" v-model.trim="email" />
-      </div>
-      <div class="form-control">
-        <label for="password">Password</label>
-        <input type="password" id="password" v-model.trim="password" />
-      </div>
-      <p
-        v-if="!formIsValid"
-      >Please enter a valid email and password (must be at least 6 characters long).</p>
-      <base-button>{{ submitButtonCaption }}</base-button>
-      <base-button type="button" mode="flat" @click="switchAuthMode">{{ switchModeButtonCaption }}</base-button>
-    </form>
-  </base-card>
+  <div>
+    <base-dialog :show="!!error" title="An error occured" @close="handleError">
+      <p>{{ error }}</p></base-dialog
+    >
+    <base-dialog :fixed="isLoading" title="Authenticating...">
+      <base-spinner></base-spinner>
+    </base-dialog>
+    <base-card>
+      <form @submit.prevent="submitForm">
+        <div class="form-control">
+          <label for="email">E-Mail</label>
+          <input type="email" id="email" v-model.trim="email" />
+        </div>
+        <div class="form-control">
+          <label for="password">Password</label>
+          <input type="password" id="password" v-model.trim="password" />
+        </div>
+        <p v-if="!formIsValid">
+          Please enter a valid email and password (must be at least 6 characters
+          long).
+        </p>
+        <base-button>{{ submitButtonCaption }}</base-button>
+        <base-button type="button" mode="flat" @click="switchAuthMode">{{
+          switchModeButtonCaption
+        }}</base-button>
+      </form>
+    </base-card>
+  </div>
 </template>
 
 <script>
@@ -26,6 +37,8 @@ export default {
       password: '',
       formIsValid: true,
       mode: 'login',
+      isLoading: false,
+      error: null
     };
   },
   computed: {
@@ -42,10 +55,10 @@ export default {
       } else {
         return 'Login instead';
       }
-    },
+    }
   },
   methods: {
-    submitForm() {
+    async submitForm() {
       this.formIsValid = true;
       if (
         this.email === '' ||
@@ -55,15 +68,23 @@ export default {
         this.formIsValid = false;
         return;
       }
+      this.isLoading = true;
 
-      if (this.mode === 'login') {
-        // ...
-      } else {
-        this.$store.dispatch('signup', {
-          email: this.email,
-          password: this.password,
-        });
+      try {
+        if (this.mode === 'login') {
+          // ...
+        } else {
+          await this.$store.dispatch('signup', {
+            email: this.email,
+            password: this.password
+          });
+        }
+      } catch (err) {
+        this.error =
+          err.message || 'Failed to authenticate, please try again later';
       }
+
+      this.isLoading = false;
     },
     switchAuthMode() {
       if (this.mode === 'login') {
@@ -72,7 +93,10 @@ export default {
         this.mode = 'login';
       }
     },
-  },
+    handleError() {
+      this.error = null;
+    }
+  }
 };
 </script>
 
