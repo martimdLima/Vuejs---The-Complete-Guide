42e2b92ddc7e0101c7e37bf840dee8beae7ec3e4
Martim Lima <martim.d.lima@protonmail.com>
2021-04-20 11:41:55 +0100

    Vue 2 - Final Project - The Stock Trader
    Creating Stocks Components

--- course-project/01-vue2/01-the-stock-trader/src/components/stocks/Stocks.vue
+++ course-project/01-vue2/01-the-stock-trader/src/components/stocks/Stocks.vue
@@ -1,3 +1,26 @@
 <template>
-    <h1>Stocks - Stocks Component</h1>
+    <div>
+       <app-stock v-for="stock in stocks"></app-stock>
+    </div>
 </template>
+
+<script>
+    import Stock from "./Stock.vue";
+
+
+export default {
+    components: {
+        appStock: Stock,
+    },
+    data() {
+        return {
+            stocks: [
+                {id: 1, name: "bmw", price: 110},
+                {id: 2, name: "tesla", price: 150},
+                {id: 3, name: "google", price: 200},
+                {id: 4, name: "J.P. Morgan", price: 210},
+            ]
+        }
+    }
+}
+</script>
